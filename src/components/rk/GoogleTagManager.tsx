277c86d4e53dcd7f86ea17239f14c949baf1cf04
Sujeet Sharma <artisticvisions003@gmail.com>
2026-09-01 15:17:58 +0530

    Refactor GoogleTagManager component
    Refactor GoogleTagManager component to remove database dependency and inline script.

--- src/components/rk/GoogleTagManager.tsx
+++ src/components/rk/GoogleTagManager.tsx
@@ -19,25 +19,4 @@ export async function GoogleTagManager() {
       })(window,document,'script','dataLayer','${gtmId}');`}
     </Script>
   );
-}import Script from "next/script";
-import { db } from "@/lib/db";
-
-export async function GoogleTagManager() {
-  const setting = await db.siteSetting.findUnique({
-    where: { key: "gtm_id" },
-  });
-
-  if (!setting?.value) return null;
-
-  const gtmId = setting.value;
-
-  return (
-    <Script id="google-tag-manager" strategy="afterInteractive">
-      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${gtmId}');`}
-    </Script>
-  );
 }
